d77081ffed1ed4d5d96a1d3da319b27e7c1bbc89
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-12-31 19:16:05 -0300

    Foreach/Arrow Function

---
+++ Praticando/script02.js
@@ -0,0 +1,22 @@
+//mostre no console cada paragrafo do site
+const paragrafo = document.querySelectorAll('p');
+console.log(paragrafo);
+
+//mostre o texto dos paragrafos no console
+paragrafo.forEach((item) => {
+  console.log(item.innerText);
+});
+
+//Como corrigir os erros abaixo:
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
+
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++);
+});
+
+imgs.forEach(() => console.log(i++));
